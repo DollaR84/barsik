b40ee4395a98a43426342233c1b7992f81325fd6
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2024-03-13 23:09:07 +0200

    restore config interface

--- barsik/config/interface.py
+++ barsik/config/interface.py
@@ -6,14 +6,6 @@ class BaseConfig:
     def __init__(self, *names: list[str], **kwargs):
         BaseConfigAdapter(self, *names, **kwargs)
 
-        self._core = None
-        self._geo = None
-        self._localisation = None
-        self._redis = None
-        self._services = None
-        self._sqlite = None
-        self._telegram = None
-
     @property
     def is_redis(self):
         return hasattr(self, "redis")
@@ -25,59 +17,3 @@ class BaseConfig:
     @property
     def is_geo(self):
         return hasattr(self, "geo")
-
-    @property
-    def core(self):
-        return self._core
-
-    @core.setter
-    def core(self, value):
-        self._core = value
-
-    @property
-    def geo(self):
-        return self._geo
-
-    @geo.setter
-    def geo(self, value):
-        self._geo = value
-
-    @property
-    def localisation(self):
-        return self._localisation
-
-    @localisation.setter
-    def localisation(self, value):
-        self._localisation = value
-
-    @property
-    def redis(self):
-        return self._redis
-
-    @redis.setter
-    def redis(self, value):
-        self._redis = value
-
-    @property
-    def services(self):
-        return self._services
-
-    @services.setter
-    def services(self, value):
-        self._services = value
-
-    @property
-    def sqlite(self):
-        return self._sqlite
-
-    @sqlite.setter
-    def sqlite(self, value):
-        self._sqlite = value
-
-    @property
-    def telegram(self):
-        return self._telegram
-
-    @telegram.setter
-    def telegram(self, value):
-        self._telegram = value
