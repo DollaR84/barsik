e6bfb88f2c8994b408d98cb2b8144f84fb74d6b2
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2026-04-04 21:32:30 +0300

    created cache function for load config one object for project

---
+++ barsik/utils/cache.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+from typing import Type, TypeVar, Any
+
+from pydantic_settings import BaseSettings
+
+
+T = TypeVar("T", bound=BaseSettings)
+
+
+@lru_cache(maxsize=1)
+def get_config(config_cls: Type[T]) -> T:
+    return config_cls()
