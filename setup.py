04d6452fca61dd00868852616c43e3e006f58350
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2026-05-01 22:18:18 +0300

    update setup version 1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='barsik',
-    version='1.3.0',
+    version='1.3.2',
     author='Ruslan Dolovaniuk',
     author_email='ruslan.dolovaniuk84@gmail.com',
     packages=find_packages(),
