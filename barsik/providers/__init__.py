b2e235f4eaba90682e4b790e63427715f88c84f6
Ruslan Dolovaniuk <ruslan.dolovaniuk84@gmail.com>
2026-05-01 21:20:02 +0300

    fixed providers init

--- barsik/providers/__init__.py
+++ barsik/providers/__init__.py
@@ -1,8 +1,14 @@
 from .bot import BotProvider
+from .config import ConfigProvider
 from .core import CoreProvider
+from .db import DBProvider
+from .redis import RedisProvider
 
 
 __all__ = [
     "BotProvider",
+    "ConfigProvider",
     "CoreProvider",
+    "DBProvider",
+    "RedisProvider",
 ]
